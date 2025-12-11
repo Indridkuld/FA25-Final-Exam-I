2409a0bd9cd1dabb808df76c859859024a3ed795
Aidan <aidanmichaelwoodcock@gmail.com>
2025-12-10 21:38:36 -0800

    Milestone 2 Complete

--- main.cpp
+++ main.cpp
@@ -25,11 +25,18 @@ int main() {
     for (const auto& entry : aiportCounts) { // Range-based for loop
         cout << entry.first << " " << entry.second << endl;
     }
-
+    // find the busiest airport by traffic count 
     int highestCount = 0;
     string busiestAirport;
 
-
+    for (const auto& entry : aiportCounts) {
+        if (entry.second > highestCount) {
+            highestCount = entry.second;
+            busiestAirport = entry.first;
+        }
+    }
+    cout << "\nBusiest Airport(s) with count " << highestCount << ": \n";
+    cout << busiestAirport << " " << highestCount << endl;
 
     return 0; 
 }
