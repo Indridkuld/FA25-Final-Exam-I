60881c0f37200d07e2a49cb5982382b788eb5c32
Aidan <aidanmichaelwoodcock@gmail.com>
2025-12-10 21:28:55 -0800

    Milestone 1 Complete

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <map> 
+#include <string>   
+
+using namespace std;
+
+int main() {
+    map<string, int> aiportCounts; 
+
+    ifstream infile("Data.txt");
+    if (!infile) {
+        cerr << "Unable to open file Data.txt";
+        return 1;
+    }
+    string destinationCode, originCode ;
+
+    while(infile >> originCode >> destinationCode) {
+        aiportCounts[originCode]++; 
+        aiportCounts[destinationCode]++;
+    }
+    infile.close();
+
+    cout << "Total Airport Traffic Counts:\n";
+    for (const auto& entry : aiportCounts) { // Range-based for loop
+        cout << entry.first << " " << entry.second << endl;
+    }
+
+
+    return 0; 
+}
